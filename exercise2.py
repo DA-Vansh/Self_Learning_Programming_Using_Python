a1c7a4fe86b8c483db8090b2dee3db5348ea84a3
Vansh N Shah <90702975+DA-Vansh@users.noreply.github.com>
2022-02-10 15:49:52 -0600

    Comment and Pound Characters
    See the script for explanations.

---
+++ exercise2.py
@@ -0,0 +1,22 @@
+# A comment, this is so you can read your command later
+# Anything after # is ignored by Python
+
+print("I could have code like this.") # and the comment after this is ignored
+
+#You can also use a comment to 'disable' or comment out the code.
+#print("This won't run.")
+
+print("But, this will run.")
+
+#I want to check it "" works within print statement
+#print("Let me see if "this" works")
+#File "C:\Users\vxshah\OneDrive - ArcelorMittal\Vansh\Training - Self_Learning_Programmer_Python\Introduction to Programming\exercise2.py", line 12
+#    print("Let me see if "this" works")
+#                          ^
+#SyntaxError: invalid syntax
+
+print ('Let me see if this "works".')   #This worked
+
+print("Lastly, lets see this 'actually' works.") # This worked too.
+
+#So, we need to be careful with the quotation makes in the print statement.
