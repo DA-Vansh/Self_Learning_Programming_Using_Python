bfda3669b7372e08e15c27f9c769122985370aba
Vansh N Shah <90702975+DA-Vansh@users.noreply.github.com>
2022-02-10 15:27:07 -0600

    A Good First Program
    writing scripts with print() statements.

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing like this")
+print("This is fun")
+print("Yay! Printing")
+print("I'd much rathe you 'not' ")
+print("I said do not touch this")
+# Here the line after \n should be printed in the next line.
+print("This is the first line.\n Let's see if this is line 2 in the output.")
