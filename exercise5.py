cc6476f12d099af40464e8eab22e7cd3d65e1a27
Vansh N Shah <90702975+DA-Vansh@users.noreply.github.com>
2022-02-24 15:09:15 -0600

    More Variables and Printing.
    Here we are learning how to embed variables inside a string using special { }  sequence and then put the variable you want inside { } characters.
    Also, check lines 21, 22 and 29. You can change the data type within { }.

---
+++ exercise5.py
@@ -0,0 +1,35 @@
+my_name = 'Vansh Shah'
+my_age = 35
+my_height = 71 #inches
+my_weight = 216 #lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'bald'
+
+print (f"Let's talk about {my_name}.")
+print (f"{my_name} is of age {my_age}.")
+print (f"I think I am over weight because I weigh {my_weight} lbs.")
+print (f"My height is {my_height} inches.")
+print (f"The color of my eyes is {my_eyes}, and I used to have black hair but currently I am {my_hair}.")
+print (f"Although I drink a lot of coffee but my teeth are still {my_teeth}.")
+
+total = my_age + my_height + my_weight
+print (f"I add {my_age}, {my_height} and {my_weight}, then I get total of {total}")
+
+name = 'Vansh Shah'
+age = 35
+height = float(71.4) #inches
+weight = float(216.2) #lbs
+eyes = 'blue'
+teeth = 'white'
+hair = 'bald'
+
+print (f"Let's talk about {name}.")
+print (f"{name} is of age {age}.")
+print (f"I think I am over weight because I weigh {int(weight)} lbs.")
+print (f"My height is {height} inches.")
+print (f"The color of my eyes is {eyes}, and I used to have black hair but currently I am {hair}.")
+print (f"Although I drink a lot of coffee but my teeth are still {teeth}.")
+
+total = age + height + weight
+print (f"I add {age}, {height} and {weight}, then I get total of {total}")
