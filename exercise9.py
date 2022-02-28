305e7e3bd546566ebdbfed19d80b9dfdfdbc1d68
Vansh N Shah <90702975+DA-Vansh@users.noreply.github.com>
2022-02-28 11:40:48 -0600

    Printing, Printing, more Printing
    Different approaches, please view the comments.

---
+++ exercise9.py
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "Jan\n Feb\n Mar\n April\n May\n June\n July\n Aug\n Sep\n Oct\n Nov\n Dec\n"
+
+print ("Here are the days:", days)
+print ("Here are the months:", months) #This will print ....months: Jan
+print ("Here are the months:\n", months) #This is print all the months in a new line
+
+# This is awesome if you want to write a paragraph and want the output the exact same way as typed.
+print("""
+There is something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want,  or 5, or 6.
+""")
