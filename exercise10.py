6c0b0c759149e4692b64be2e61925ca350a59828
Vansh N Shah <90702975+DA-Vansh@users.noreply.github.com>
2022-02-28 11:53:33 -0600

    What was that?
    In this exercise,
    ( \ ) backslash character encodes difficult-to-type characters to a string. 
    An important escape sequence is to escape a single quote ( ' ) or double quote ( " )
    
    "I am 6 ' 2 \ " tall." # escape double-quotes inside string
    'I am 6\ ' 2 " tall.' # escape single-quotes inside string

---
+++ exercise10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'am tabbed in. " #Enters tab
+persian_cat = "I'm split \non a line." #text after \n goes to a new line
+backslash_cat = "I'am \\ a \\ cat." # put \ in between texts
+
+#Create a list.
+fat_cat = """   
+I'll do a list:
+\t* Cat Food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
