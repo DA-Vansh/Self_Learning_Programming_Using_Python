64bc32f5007d0dfd48c5860e096a45416d231493
Vansh N Shah <90702975+DA-Vansh@users.noreply.github.com>
2022-03-05 13:47:25 -0600

    Asking Questions
    Here we are using the input() function to ask questions in different ways.
    Also, here at the end of the print statements we have used (end = ' '). This tells print to not end the line with a newline character and go to the next line.

---
+++ exercise11.py
@@ -0,0 +1,19 @@
+print("How old are you?", end = '')
+age = input()
+
+print ("How tall are you?", end = '')
+height = input()
+
+print ("How much do you weigh?", end = '')
+weight = input()
+
+print (f"So you're {age} old, {height} tall and {weight} heavy.")
+
+# More practice
+
+print ("Can you please tell me your name?", end = '')   # Basic Print statement
+x = input()                                             # Assigning the input value to x
+print ("Thank you, " + x )                             # Printing the input variable in a sentence35
+
+y = input('Please enter your email address:')
+print (f"Thank you {x}, we will email you at: " +y)
