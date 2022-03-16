ad17bf83218d5f1498eea2895f576b2bf81a0c68
Vansh N Shah <90702975+DA-Vansh@users.noreply.github.com>
2022-03-16 09:17:56 -0500

    Name, Variables, Code, Function

--- exercise18.py
+++ exercise18.py
@@ -1,5 +1,6 @@
 # this one is like your scripts with argv
-def print_two (*args):
+def print_two (*args):                                          #Here we are telling Python to make a function using def for "define" and give the function a name (print_two). We passed *argv parameter and ended the line using ':'
+# After the colon (:), all the lines that are indented four spaces will become attached to print_two. The below indented line is the one that unpacks the arguments, the same as with your script.
     arg1, arg2 = args
     print (f"arg1 : {arg1}, arg2: {arg2}")
 
@@ -20,3 +21,5 @@ print_two("Vansh", "Shah")
 print_two_again("Vansh", "Shah")
 print_one("First!")
 print_none()
+
+#***#NOTE:What does * in *args do? That tells Python to take all the arguments to the function and then put them in args list. It's like argv that you've been using but for functions.  
